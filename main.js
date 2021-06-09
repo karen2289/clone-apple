9290a285a5299917428a4f43151ed90237c9e26b
Karen Aguirre <latingirl_2289@hotmail.com>
2021-06-09 13:22:38 +0200

    clone apple

---
+++ main.js
@@ -0,0 +1,7 @@
+
+/*  MENU MOBILE */
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () => {
+    selectElement('header').classList.toggle('active');
+});
